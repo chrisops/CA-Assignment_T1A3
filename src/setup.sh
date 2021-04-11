94eba86a5ba3536e896444a67694c59b8540c036
chris <homebase.op@gmail.com>
2021-04-11 20:47:49 +1000

    fixed another massive bug -resubmitting right before deadline

--- src/setup.sh
+++ src/setup.sh
@@ -4,6 +4,7 @@ printf "Installing bundled gems...\n\n"
 sleep 1
 bundle install
 
+
 printf "\n\n**Invoice sending uses a mailjet API, Please go to mailjet.com and register for an API key, then enter your API key:**\n"
 printf "API key:   "
 read apikey
@@ -12,6 +13,13 @@ read secretkey
 printf "Email address:   "
 read email
 
+if [ ! -z $apikey ] && [ ! -z $secretkey ] && [ ! -z $email ]
+then
 printf "APIKEY=$apikey\n" > .env
-printf "APISECRET=$secretkey" >> .env
+printf "APISECRET=$secretkey\n" >> .env
 printf "EMAIL=$email" >> .env
+else
+echo "Empty values not valid, run setup.sh again to reconfigure email"
+fi
+
+printf "\n**If there were any errors during setup, run:\n\nsudo apt-get install ruby-dev\n\nthen run bash setup.sh again to fix**\n\n"
